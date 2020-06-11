afc5e6c75254aa77d2ea8ed4c50aa6738528089d
Enes Can <1039335@mborijnland.nl>
2020-06-11 15:25:48 +0200

    added URL asset to css in header, added google font and specific hex of yellow colour

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,14 +3,16 @@
 @section('title', 'Home Page')
 
 @section('content')
-    <div class="container">
-        <div class="row text-center">
-            <div class="col">
-                1 of 2
-            </div>
-            <div class="col">
-                2 of 2
+    <main>
+        <div class="container">
+            <div class="row text-center">
+                <div class="col">
+                    1 of 2
+                </div>
+                <div class="col">
+                    2 of 2
+                </div>
             </div>
         </div>
-    </div>
+    </main>
 @endsection
