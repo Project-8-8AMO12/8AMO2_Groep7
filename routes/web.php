e72ac3bc70496a6d3260ce786119da6303816b5c
Enes Can <1039335@mborijnland.nl>
2020-06-11 14:00:38 +0200

    added routes for pages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,52 @@
 |
 */
 
+// Missing login/registration page and route
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/activiteiten', function () {
+    return view('pages/activiteiten');
+});
+
+Route::get('/agenda', function () {
+    return view('pages/agenda');
+});
+
+Route::get('/bijenstal', function () {
+    return view('pages/bijenstal');
+});
+
+Route::get('/contact', function () {
+    return view('pages/contact');
+});
+
+Route::get('/cursussen', function () {
+    return view('pages/cursussen');
+});
+
+Route::get('/foto', function () {
+    return view('pages/foto');
+});
+
+Route::get('/nieuws', function () {
+    return view('pages/nieuws');
+});
+
+Route::get('/stertselaar', function () {
+    return view('pages/stertselaar');
+});
+
+Route::get('/winkel', function () {
+    return view('pages/winkel');
+});
+
+Route::get('/zwermgezien', function () {
+    return view('pages/zwermgezien');
+});
+
+Route::get('/', function () {
+    return view('pages/');
 });
