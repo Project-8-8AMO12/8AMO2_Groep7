f308812ec1813af1faa182faab431ab33d677212
Enes Can <1039335@mborijnland.nl>
2020-06-22 00:04:54 +0200

    adjusted header and footer to match styling of the page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,10 @@
 @section('footer')
-    <footer class="footer mt-auto py-3 fixed-bottom">
-        <div class="container">
-            <span class="text-muted">Place sticky footer content here.</span>
+    <footer class="footer fixed-bottom text-white" style="background-color: #ffd73d;">
+        <div class="footer-copyright p-3">
+            <a href="/" class="text-white"> <i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a href="/" class="text-white"> <i class="fa fa-facebook" aria-hidden="true"></i></i></a>
+            <a href="/" class="text-white"> <i class="fa fa-at" aria-hidden="true"></i></a>
+            <a href="/" style="color: #ffffff;" class="float-right"> 2020 Copyright ImkersLeiden</a>
         </div>
     </footer>
 @show
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,34 +1,46 @@
 @section('header')
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Navbar</a>
+    <nav class="navbar navbar-expand-md " style="background-color: #ffd73d;">
+        <a class="navbar-brand" href="/"><img src="https://www.imkersleiden.nl/wp-content/uploads/2019/07/Logo-Imkers-Leiden.svg" width="175px" alt="Logo" class="d-inline-block align-middle mr-2"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon">
+                    <i class="fas fa-bars" style="color:#fff; font-size:28px;"></i>
+                </span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link text-white" href="/activiteiten">Activiteiten</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    <a class="nav-link text-white" href="/agenda">Agenda</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown01">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/cursussen">Cursussen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/contact">Contact</a>
+                </li>
+                <div class="dropdown">
+                    <a class=" btn btn-secondary dropdown-toggle" style="background-color: #ffd73d; border-color: #ffd73d; padding-top: 7.7px;" href="/" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Overige
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink" style="background-color: #ffd73d">
+                        <a class="nav-link text-white" href="/bijenstal">Bijenstal</a>
+                        <a class="nav-link text-white" href="/foto">Foto</a>
+                        <a class="nav-link text-white" href="/nieuws">Nieuws</a>
+                        <a class="nav-link text-white" href="/stertselaar">Sterselaar</a>
+                        <a class="nav-link text-white" href="/vereniging">Vereniging</a>
+                        <a class="nav-link text-white" href="/winkel">Winkel</a>
+                        <a class="nav-link text-white" href="/zwermgezien">Zwerm Gezien</a>
                     </div>
+                </div>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="/login"> <i class="fas fa-user text-white"></i> Inloggen/Registreren</a>
                 </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-                <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-            </form>
         </div>
     </nav>
 @show
