f5cbdb8e08878e5c2d2b61f28df5dbe0c2c3cfca
Enes Can <1039335@mborijnland.nl>
2020-06-21 23:59:54 +0200

    added index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,15 +4,53 @@
 
 @section('content')
     <main>
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row text-center mt-3">
+                    <div class="col-md-6 text-center">
+                        {{$loremipsum}}
+                    </div>
+                    <div class="col-md-6 text-center">
+                        <img src="/img/bijenhouder.jpg" class="rounded img-fluid">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col-md-6 text-center">
+                        <img src="/img/bijenhouder.jpg" class="rounded img-fluid">
+                    </div>
+                    <div class="col-md-6 text-center">
+                        {{$loremipsum}}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid">
+            <div class="container">
+                <div class="row text-center">
+                    <div class=" col-md-4 p-2">
+                        <button type="button" onclick="window.location.href='/activiteiten'"class="btn btn-primary" style="background-color: #ffd73d; border-color: #ffd73d;">Activiteiten</button>
+                    </div>
+                    <div class="col-md-4 p-2">
+                        <button type="button" onclick="window.location.href='/login'" class="btn btn-primary" style="background-color: #ffd73d; border-color: #ffd73d;">Inloggen/Registreren</button>
+                    </div>
+                    <div class="col-md-4 p-2">
+                        <button type="button" onclick="window.location.href='/contact'" class="btn btn-primary" style="background-color: #ffd73d; border-color: #ffd73d;">Contact Us</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
         <div class="container">
             <div class="row text-center">
-                <div class="col">
-                    1 of 2
+                <div class="col-md-3"></div>
+                <div class="col-md-6 text-center">
+                    <img src="/img/honey.jpg" class="rounded img-fluid">
                 </div>
-                <div class="col">
-                    2 of 2
-                    {{ $loremipsum }}
-                </div>
+                <div class="col-md-3"></div>
             </div>
         </div>
     </main>
